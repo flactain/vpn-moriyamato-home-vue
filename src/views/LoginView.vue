7128bd9d30a3109f0c6c23f43fccdcc2fe85ccc7
moriyamato <flactly@gmail.com>
2025-01-07 02:16:51 +0900

    feat: 一旦ログインまではできるようにはなった

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,35 @@
 <script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+
+const client_id = import.meta.env.VITE_CLIENT_ID
+const github_base_url = import.meta.env.VITE_GITHUB_OAUTH_URL
+const github_url = import.meta.env.VITE_GITHUB_OAUTH_URL + '?client_id=' + client_id + '&scope=user'
+
+async function getGitHubId() {
+    await axios.get(github_base_url, {
+        params: {
+            client_id: client_id,
+            redirect_uri: 'http://localhost:5173/',
+            scope: 'user'
+        }
+    }).then(function (response) {
+        console.log(response);
+
+    }).catch(function (error) {
+        console.log(error);
+
+    })
+}
 </script>
 
 <template>
-<h1>you must login</h1>
+    <div>
+
+        <h1>you must login</h1>
+    </div>
+    <div>
+        <a :href="github_url">loooogin</a>
+    </div>
 </template>
---
+++ src/views/LoginWait.vue
@@ -0,0 +1,11 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+const github_url = import.meta.env.VITE_GITHUB_OAUTH_URL
+
+</script>
+
+<template>
+    <h1>wait</h1>
+</template>
